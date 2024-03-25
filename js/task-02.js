38159f4411d3fb9e3ae35a888e0a237e64def0db
litoy-ko <litoys.ark2nd@gmail.com>
2024-03-25 20:01:16 +0800

    Update task-02.js
    update task numbe 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.getElementById("ingredients");
+
+for (let ingredient of ingredients) {
+  let li = document.createElement("");
+  li.innerText = ingredient;
+  ul.appendChild(li);
+}
