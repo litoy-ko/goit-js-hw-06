889c497fdadcbcacb3e4cd7545f2d7ad3d1f1144
litoy-ko <litoys.ark2nd@gmail.com>
2024-03-28 23:03:39 +0800

    Update task-08.js
    done task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+let form = document.querySelector(".login-form");
+let inputs = document.querySelectorAll("input");
+let person = new Object();
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // console.log(inputs.length);
+    for (let input of inputs) {
+        if (input.value === "") {
+            alert("All fields must be filled in.")
+            return;
+        }
+        else {
+            let prop = input.getAttribute("name");
+            person[prop] = input.value;
+        }
+    }
+    console.log(person);
+    e.target.reset();
+})
 
