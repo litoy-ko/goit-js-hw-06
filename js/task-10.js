8b4e3defaf993f8bbdccfcd6381d6a7ff7364938
litoy-ko <litoys.ark2nd@gmail.com>
2024-03-30 10:53:56 +0800

    update task 10
    Done hw6

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+let input = document.querySelector("[type='number']");
+let btncreate = document.querySelector("[data-create]")
+let btndestroy = document.querySelector("[data-destroy]")
+let boxes = document.getElementById("boxes");
+
+// console.log(btncreate);
+
+btncreate.addEventListener("click", () => {
+  // console.log(input.value);
+  let amount = input.value;
+  createBoxes(amount);
+})
+
+btndestroy.addEventListener("click", () => {
+  // boxes.innerHTML = "";
+  // input.value = ""
+  location.reload();
+})
+
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i++) {
+    let color = getRandomHexColor();
+    let box = document.createElement("div");
+    box.id = "box" + i;
+    box.style.backgroundColor = color;
+    box.style.width = "30px";
+    box.style.height = "30px";
+    boxes.appendChild(box);
+  }
+}
